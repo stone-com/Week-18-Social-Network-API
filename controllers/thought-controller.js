a25df459194ad81c6e84960faca30cc16ac588e3
Stone Comstock <comstockstone@yahoo.com>
2022-04-25 21:42:08 -0700

    Thhought controller done

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -65,4 +65,69 @@ const thoughtController = {
       })
       .catch((err) => res.json(err));
   },
+  // update a thought by its ID
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((results) => {
+        // if no results, set status 404 and message user
+        if (!results) {
+          res
+            .status(404)
+            .json({ message: 'No user found, try again with different ID' });
+          return;
+        }
+        // else, respond with results
+        res.json(results);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  // delete a thought by its ID
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then((results) => {
+        // if no results, set status 404 and message user
+        if (!results) {
+          res.status(404).json({
+            message: 'No results found with that ID, try a different one',
+          });
+          return;
+        }
+        // else respond with results
+        res.json(results);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  // add a new reaction
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $addToSet: { reactions: body } },
+      { new: true }
+    )
+      .then((results) => {
+        //   if no results, set status 404 and message user
+        if (!results) {
+          res
+            .status(404)
+            .json({ message: 'No results found, try again with different ID' });
+        }
+        //   else respond with results
+        res.json(results);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // delete a reaction
+  deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((results) => res.json(results))
+      .catch((err) => res.status(400).json(err));
+  },
 };
+
+// export
+module.exports = thoughtController;
